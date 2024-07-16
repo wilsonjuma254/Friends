2c8418b56947e11ee9ddcb324c240afafeeee7ef
Wilsonmobisa <willexjumah12338@gmail.com>
2024-07-16 22:42:10 +0300

    updating friends backend

--- friends.php
+++ friends.php
@@ -1,8 +1,15 @@
 <?php
-if(isset($_post['submit'])){
-	$lname = $_post['lname'];
-	$friends = $_post['friends'];
-	echo "Your name is: $lname <br>";
-	echo "Your comment is: $friends";
+// if(isset($_post['submit'])){
+// 	$lname = $_post['lname'];
+// 	$friends = $_post['friends'];
+// 	echo "Your name is: $lname <br>";
+// 	echo "Your comment is: $friends";
+// }
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$name = $_POST['uname'];
+	$comment = $_POST['friends'];
+	echo "Name: $name <br>";
+	echo "Comment: $comment";
 }
 ?>
